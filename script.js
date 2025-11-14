a3367f71ae5ebd4c36c0e229f54dde2d85febb41
Illia Tarabarov <tarabarov.illia@gmail.com>
2025-11-14 22:20:34 +0100

    Tarabarov-Portfolio-1

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelector(".email").addEventListener("click", function () {
+  window.location.href = "mailto:tarabarov.illia@gmail.com";
+});
+document.querySelector(".tel").addEventListener("click", function () {
+  window.location.href = "tel:+420770324457";
+});
+document.querySelector(".github").addEventListener("click", function () {
+  window.open("https://github.com/Illia-Tarabarov");
+});
+document.querySelector(".insta").addEventListener("click", function () {
+  window.open("https://www.instagram.com/alkatpa3/");
+});
+document.querySelector(".linkedin").addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/illia-tarabarov-1bb4a6358/");
+});
+document.querySelector(".city").addEventListener("click", function () {
+  window.open(
+    "https://www.google.com/maps/place/%C4%8Cesk%C3%A9+Bud%C4%9Bjovice/@48.976305,14.1792429,11z/data=!3m1!4b1!4m14!1m7!3m6!1s0x47734f4f93d131bb:0x31141dc35a0016eb!2sLion+Sport+s.r.o.+-+zna%C4%8Dkov%C3%A1+prodejna+adidas,+Reebok,+Puma!8m2!3d48.9885397!4d14.4737419!16s%2Fg%2F11jybkj2cj!3m5!1s0x47734fb43a5f629b:0x400af0f6614de80!8m2!3d48.9756578!4d14.480255!16zL20vMDEzNXFi?entry=ttu&g_ep=EgoyMDI1MTEwOS4wIKXMDSoASAFQAw%3D%3D",
+    "_blank"
+  );
+});
+// -------------------
